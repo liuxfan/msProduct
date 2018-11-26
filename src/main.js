82c83463f7d4f9f262f9f2b18a3b1b83b3c3c6d1
lixiaoyun <lxyfan1123@163.com>
2018-11-26 10:32:10 +0800

    增加app.json

--- src/main.js
+++ src/main.js
@@ -7,17 +7,17 @@ App.mpType = 'app'
 const app = new Vue(App)
 app.$mount()
 
-export default {
-    config:{
-        // ^为首页
-        pages: ['pages/logs/main', '^pages/index/main', "pages/counter/main"
-          ,'pages/addResume/main'
-        ],
-        window:{
-            backgroundTextstyle:'light',
-            navigationBarBackgroundColor:'#fff',
-            NavigationBarTitleText:'WeChat',
-            navigationBarTextStyle:'black'
-        }
-    }
-}
+// export default {
+//     config:{
+//         // ^为首页
+//         pages: ['pages/logs/main', '^pages/index/main', "pages/counter/main"
+//           ,'pages/addResume/main'
+//         ],
+//         window:{
+//             backgroundTextstyle:'light',
+//             navigationBarBackgroundColor:'#fff',
+//             NavigationBarTitleText:'WeChat',
+//             navigationBarTextStyle:'black'
+//         }
+//     }
+// }
