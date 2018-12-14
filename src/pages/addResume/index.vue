eda6ede32354c7b19f75b8656f6b8ef78a53adc6
lixiaoyun <lxyfan1123@163.com>
2018-12-14 17:47:19 +0800

    更新新增页数据

--- src/pages/addResume/index.vue
+++ src/pages/addResume/index.vue
@@ -1,18 +1,26 @@
 <template>
     <div>
-        <picker @change="bindPickerChange" :value="index" :range="fruit">
+        <picker @change="bindPickerChange" :value="index" :range="labelList">
             <div class="picker">
-                {{fruit[index]}}<i-icon type="unfold" size="28" color="#80848f" />
+                {{labelList[index]}}<i-icon type="unfold" size="25" color="#80848f" />
             </div>
         </picker>
-        <i-button  @click="addTitle" class="addTitle">新增标签</i-button>
-        <i-modal title="编辑标题" :visible="visible" @ok="handleConfirm" @cancel="handleClose">
+        <button  @click="addLabelTitle" class="addLabelTitle">标题</button>
+        <i-modal title="编辑标题" :visible="visible"  @ok="handleConfirm" @cancel="handleClose">
              <i-radio-group  :current="current"  @change="handleChange">
-                <i-radio v-for="item in country"  :key="item.id" :value="item.value">{{item.name}}</i-radio>
-                 <i-radio :value="inputValue"></i-radio>
-                 <input v-model="inputValue" type="text" autofocus placeholder="自定义添加" /> 
+                <i-radio v-for="item in titleList"  :key="item.id" :value="item.titileName">
+                    {{item.titileName}}
+                </i-radio>
             </i-radio-group>
         </i-modal>
+        <!-- <button  @click="contentTitle" class="addTitle">内容</button>
+        <i-modal title="编辑内容" :visible="visible" @ok="confirm" @cancel="close">
+             <i-radio-group  :current="current"  @change="change">
+                <i-radio v-for="item in contentList"  :key="item" :value="item">
+                    {{item}}
+                </i-radio>
+            </i-radio-group>
+        </i-modal> -->
     </div>
 </template>
 
@@ -22,49 +30,134 @@ export default {
     return {
       list:'',
       inputValue:'',
+      labelList:[],//标签列表
+      index: 0, //下拉菜单默认项
       visible: false,
-      country: [ 
-        {id:0,name: 'CHN', value: '中国'},
-        {id:1,name: 'USA', value: '美国'},
-        {id:2,name: 'BRA', value: '巴西'},
-        {id:3,name: 'JPN', value: '日本'}
-      ],
-      current: '中国',
-      fruit: ['苹果', '香蕉', '橘子', '西瓜'],
-      index: 1 //下拉菜单默认项
+      titleList:[],//标题列表
+      current:'我是谁',
+      contentList:[],//内容列表
+      
     }
   },
+  mounted(){
+      //标签列表
+      wx.request({
+            url: 'https://www.zihaoapi.cn/resume/min-lable/findList',
+            data: {
+            },
+            header: {'content-type':'application/json'},
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'text',
+            success: (res)=>{
+                console.log("uuuuuuuuu",res)
+                if (res.data.code == 1) {
+                    let array=[];
+                    for(var i=0;i<res.data.data.length;i++){
+                        array.push(res.data.data[i].labelName);
+                        this.labelList=array;
+                    }
+                }
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+        })
+      
+  },
+  created () {
+    // 调用应用实例的方法获取全局数据
+
+  },
   methods: {
-    addTitle:function () {
+    // 新增标题列表
+    addLabelTitle:function () {
         this.visible=true;
+         wx.request({
+            url: 'https://www.zihaoapi.cn/resume/min-title/findList',
+            data: {
+            },
+            header: {'content-type':'application/json'},
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'text',
+            success: (res)=>{
+                console.log("qqqqqqqqqqq",res)
+                if (res.data.code == 1) {
+                    this.titleList=res.data.data;
+                }
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+        })
+      
     },
+    // 新增内容列表
+    contentTitle:function () {
+        this.visible=true;
+         wx.request({
+            url: 'https://www.zihaoapi.cn/resume/min-content/findFieldNameList',
+            data: {
+            },
+            header: {'content-type':'application/json'},
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'text',
+            success: (res)=>{
+                console.log("内容2222222222",res.data.data)
+                if (res.data.code == 1) {
+                    this.contentList=res.data.data;
+                    console.log(this.contentList)
+                }
+            },
+            fail: ()=>{},
+            complete: ()=>{}
+        })
+      
+    },
+
     handleClose(){
         this.visible=false;
     },
     handleConfirm(){
         this.visible=false;
-        this.country.push(this.inputValue)       
-        // console.log("aaaaaa",this.list)
-        console.log("77777",this.country)
-
-
+        // this.titleList.push(this.inputValue); 
+        // console.log("77777777777",this.titleList)   
     },
     handleChange(e){
         this.current= e.mp.detail.value
     },
     bindPickerChange: function(e) {
-        console.log('picker发送选择改变，携带值为', e.mp.detail.value)
-        this.index=e.mp.detail.value
+        console.log('picker发送选择改变，携带值为', e)
+        this.index=e.mp.detail.value;
     }
   },
-  mounted(){
-      
-  },
-  created () {
-    // 调用应用实例的方法获取全局数据
-  }
+  
 }
 </script>
 <style scoped>
+.picker{
+    position: fixed;
+    right:40rpx;
+    top:10rpx;
 
+}
+.addLabelTitle{
+    position: absolute;
+    width:150rpx;
+    height:80rpx;
+    line-height: 80rpx;
+    top:80rpx;
+    right:60rpx;
+    font-size:38rpx;
+}
+.addTitle{
+    position: absolute;
+    width:150rpx;
+    height:80rpx;
+    line-height: 80rpx;
+    top:180rpx;
+    right:60rpx;
+    font-size:38rpx;
+
+}
 </style>
